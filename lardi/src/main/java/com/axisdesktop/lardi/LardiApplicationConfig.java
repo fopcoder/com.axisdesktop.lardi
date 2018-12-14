b2e5c81cc110c91cfd82027379f3b2aa8aa35385
fxcoder <barabass@gmail.com>
2018-12-15 01:06:06 +0200

    markup, minor changes

--- lardi/src/main/java/com/axisdesktop/lardi/LardiApplicationConfig.java
+++ lardi/src/main/java/com/axisdesktop/lardi/LardiApplicationConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.Validator;
+import com.axisdesktop.lardi.validator.ContactValidator;
 import com.axisdesktop.lardi.validator.UserValidator;
 
 @Configuration
@@ -17,5 +18,10 @@ public class LardiApplicationConfig {
   @Bean
   public Validator userValidator() {
     return new UserValidator();
+  }
+
+  @Bean
+  public Validator contactValidator() {
+    return new ContactValidator();
   }
 }
